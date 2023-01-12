dd8104d72059d58bdac039c68fdde3c23c727e76
DarkUsagi13 <hector.martinez.cornejo.al@iespoligonosur.org>
2023-01-13 00:30:38 +0100

    Boletin 6

---
+++ Boletin 6/ej6.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', ()=>{
+    let contenedor = document.getElementsByTagName('div')[0];
+    let enlace = document.createElement('a');
+    let texto = document.createTextNode('Mi enlace');
+    enlace.appendChild(texto);
+    enlace.setAttribute('href', '#');
+    enlace.setAttribute('target', '_self');
+    contenedor.appendChild(enlace);
+    let div = document.createElement('div')
+
+    let valores = document.getElementsByTagName('a')[0];
+    //console.log(valores.attributes)
+    for (let i = 0; i < valores.attributes.length; i++) {
+        let p = document.createElement('p')
+        let attr = document.createTextNode(`${valores.attributes[i].value}`);      
+        p.appendChild(attr)
+        div.appendChild(p)
+    }
+    contenedor.appendChild(div)
+})
